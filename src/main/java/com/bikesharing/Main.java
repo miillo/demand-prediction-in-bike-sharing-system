1fdea7e644cf75cf90d172ebd3009333ef63321d
Miłosz Pluta <milosz.pluta2@gmail.com>
2020-01-03 12:49:38 +0100

    SystemSupervisor actor

--- src/main/java/com/bikesharing/Main.java
+++ src/main/java/com/bikesharing/Main.java
@@ -1,6 +1,6 @@
 package com.bikesharing;
 
-public class BikeSharing {
+public class Main {
     public static void main(String[] args) {
         System.out.println("WWW");
     }
---
+++ src/main/java/com/bikesharing/SystemSupervisor.java
@@ -0,0 +1,32 @@
+package com.bikesharing;
+
+import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class SystemSupervisor extends AbstractBehavior<Void> {
+
+    private SystemSupervisor(ActorContext<Void> context) {
+        super(context);
+        context.getLog().info("Application started");
+    }
+
+    public static Behavior<Void> create() {
+        return Behaviors.setup(SystemSupervisor::new);
+    }
+
+    @Override
+    public Receive<Void> createReceive() {
+        return newReceiveBuilder()
+                .onSignal(PostStop.class, signal -> onPostStop())
+                .build();
+    }
+
+    private SystemSupervisor onPostStop() {
+        getContext().getLog().info("Application stopped");
+        return this;
+    }
+}
